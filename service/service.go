6f3485f3f14aa4aa894936274ef41c335dd16f75
Yuri Chandra <yurichandra@gmail.com>
2019-08-25 13:28:37 +0700

    Update dependency injection

--- service/service.go
+++ service/service.go
@@ -1,10 +1,21 @@
 package service
 
-import "github.com/go-redis/redis"
+import (
+	"github.com/go-redis/redis"
+	"github.com/yurichandra/shrt/repository"
+)
 
 // InitRedisService initialize redis service.
 func InitRedisService(client *redis.Client) *RedisService {
 	return &RedisService{
 		client: client,
+	}
+}
+
+// InitURLService initialize url service.
+func InitURLService(repo repository.URLRepositoryContract, redis RedisServiceContract) *URLService {
+	return &URLService{
+		repo:  repo,
+		cache: redis,
 	}
 }
